889f299e53390dad15064143178c9b91d60f7c4b
Pievisco89 <pie-89@live.it>
2021-04-09 15:46:36 +0200

    creato lista

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* creare e stampare num da 1 a 100 */
+for(var i = 1; i <= 100; i++){
+  console.log(i);
+  if(i%3 == 0){
+    console.log("Fuzz")
+  }
+}
+
+/* multipi di 3 stampare "fuzz" - multipi di 5 stampare "buzz"*/
+
