220d8ad90d2df5f1d7b03aab961828bb9b56aaf4
Pievisco89 <pie-89@live.it>
2021-04-09 16:10:19 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 /* creare e stampare num da 1 a 100 */
 for(var i = 1; i <= 100; i++){
-  console.log(i);
-  if(i%3 == 0){
-    console.log("Fuzz")
+  if(i%3 == 0 && i%5 == 0){
+    console.log("FizzBuzz")
+  }else if(i%3 == 0){
+    console.log("Fizz")
+  }else if(i%5 == 0){
+    console.log("Buzz")
+  }else{
+    console.log(i)
   }
 }
-
-/* multipi di 3 stampare "fuzz" - multipi di 5 stampare "buzz"*/
+/* multipi di 3 stampare "fizz" - multipi di 5 stampare "buzz - multipli entrambi "fizzbuzz"*/
 
